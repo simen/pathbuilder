3df417d4a28bc63b8c69e44ce9fa405fc3e6882b
Simen Svale Skogsrud <simen@bengler.no>
2011-09-15 23:22:22 +0200

    added invocation handling hook

--- lib/uribuilder.rb
+++ lib/uribuilder.rb
@@ -3,12 +3,21 @@ class UriBuilder
     @path = []
   end
 
-  def method_missing(method, *args)
-    @path << "#{method}"
+  def [](*args)
     args.each do |arg|
       @path << arg
     end
     self
+  end
+
+  def method_missing(method, *args)
+    @path << "#{method}"
+    arg_handler(*args) unless args.empty?
+    self
+  end
+
+  def arg_handler(*args)
+    # override this to handle invocations
   end
 
   def to_path
